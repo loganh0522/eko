f47fe1764a20bf894aa258b95d74541682435cca
loganh0522 <loganh0522@gmail.com>
2015-11-10 13:54:11 -0500

    Added the create new Company action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,6 +5,19 @@ class CompaniesController < ApplicationController
   end
 
   def create 
+    @company = Company.new(company_params)
 
+    if @company.save? 
+      flash[:notice] = "Thanks for joining #{@company.name}"
+      redirect_to backend_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def company_params 
+    params.require(:company).permit(:name, :website)
+  end 
 end
