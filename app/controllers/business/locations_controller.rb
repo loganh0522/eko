2fde9750342de545b7c4e00b4a324f80b8e342ef
loganh0522 <loganh0522@gmail.com>
2015-11-26 14:36:26 -0500

    added new action

--- app/controllers/business/locations_controller.rb
+++ app/controllers/business/locations_controller.rb
@@ -1,7 +1,14 @@
 class Business::LocationsController < ApplicationController
+  before_filter :require_user
+  before_filter :belongs_to_company
+
   def index
     @subsidiaries = current_company.subsidiaries
     @company = current_company
+  end
+
+  def new 
+    @location = Location.new
   end
   
   def show
