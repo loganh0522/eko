51eac337a767f61b86dd6d94ad352a8859f5a7d9
loganh0522 <loganh0522@gmail.com>
2016-02-02 12:26:38 -0500

    changed job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,16 +3,6 @@ class Job < ActiveRecord::Base
 
   has_many :hiring_teams
   has_many :users, through: :hiring_teams
-<<<<<<< HEAD
-  has_many :stages
-
-  has_many :applications 
-  has_many :applicants, class_name: "User", through: :applications
-  
-  validates_presence_of :title, :benefits, :description, :country, :city, :province
-
-  # attr_reader :user_tokens
-=======
   has_many :stages, -> {order(:position)}
   
   validates_presence_of :title, :benefits, :description, :country, :city, :province
@@ -26,9 +16,5 @@ class Job < ActiveRecord::Base
   def user_tokens=(ids)
     self.user_ids = ids.split(',') 
   end
->>>>>>> create_job
 
-  # def user_tokens=(ids)
-  #   self.user_ids = ids.split(',') 
-  # end
 end
