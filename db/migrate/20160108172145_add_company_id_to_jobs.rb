6f2359c8bf22c5419f30b2e564049ec9c65ea41a
loganh0522 <loganh0522@gmail.com>
2016-05-17 07:35:47 -0400

    changing migrations

--- db/migrate/20160108172145_add_company_id_to_jobs.rb
+++ db/migrate/20160108172145_add_company_id_to_jobs.rb
@@ -1,6 +1,5 @@
 class AddCompanyIdToJobs < ActiveRecord::Migration
   def change
-    add_column :job_postings, :company_id, :integer
     add_column :job_postings, :created_at, :datetime
     add_column :job_postings, :updated_at, :datetime
   end
