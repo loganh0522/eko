60e3a96aa76dd210685f7b3e0d30b15496888ece
loganh0522 <loganh0522@gmail.com>
2016-06-01 08:32:50 -0400

    Finished javascript to hide/show closed, open, draft and archived jobs

---
+++ db/migrate/20160531225136_add_status_column_to_job.rb
@@ -0,0 +1,5 @@
+class AddStatusColumnToJob < ActiveRecord::Migration
+  def change
+    add_column :jobs, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530191228) do
+ActiveRecord::Schema.define(version: 20160531225136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,7 @@ ActiveRecord::Schema.define(version: 20160530191228) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
+    t.string   "status"
   end
 
   create_table "locations", force: :cascade do |t|
