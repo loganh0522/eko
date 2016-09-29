21f126559964bf6893ae3e3b2698bd7a3bce079a
loganh0522 <loganh0522@gmail.com>
2016-09-29 08:45:24 -0400

    added protected to widgets controller

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,6 +3,8 @@ class WidgetsController < ApplicationController
 
   # before_filter :validate_api_key, :only => [:company_jobs]
 
+  protected 
+  
   def company_jobs
     @company = Company.where(widget_key: params[:widget_key])
     @job_board = @company.first.job_board
