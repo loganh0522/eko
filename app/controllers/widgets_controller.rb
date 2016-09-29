13294e73be0227c409d58fe4a1698f7c47e592b8
loganh0522 <loganh0522@gmail.com>
2016-09-29 08:50:13 -0400

    add cross_domain_protection

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,12 +3,17 @@ class WidgetsController < ApplicationController
 
   # before_filter :validate_api_key, :only => [:company_jobs]
 
-  protected 
-  
+  protect_from_forgery except: :company_jobs
+
   def company_jobs
     @company = Company.where(widget_key: params[:widget_key])
     @job_board = @company.first.job_board
     @jobs = @company.first.jobs
   end
 
+  # protected 
+
+  # def validate_api_key 
+
+  # end
 end
