25775cc1f66a3b57c96bf4302a98c121f7c60f9e
loganh0522 <loganh0522@gmail.com>
2016-09-30 15:37:17 -0400

    changed params for inbound messsages

--- app/controllers/inbound_emails_controller.rb
+++ app/controllers/inbound_emails_controller.rb
@@ -4,10 +4,12 @@ class InboundEmailsController < ApplicationController
   def create
     @token = params[:recipient].split('-').last.split('@').first
     @application = Application.find_by(token: @token)
-    @msg_body = params["body-plain"]
+    @msg_body = params["stripped-text"]
 
     if @application.present? 
       @message = Message.create(body: @msg_body, application_id: @application.id, user_id: @application.user_id)
     end
+
+    head 200
   end
 end
