31cbe344b493dd1bcb85dd7cd4a70e581e65a128
loganh0522 <loganh0522@gmail.com>
2016-09-30 14:30:00 -0400

    changed the production mailign environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'prod-talentwiz.heroku.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain 
   }
 end
