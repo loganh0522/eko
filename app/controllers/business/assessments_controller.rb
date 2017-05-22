68115b868c4727c676d16204353fcd641f4fdef8
loganh0522 <loganh0522@gmail.com>
2017-05-22 12:26:23 -0400

    changes

--- app/controllers/business/assessments_controller.rb
+++
@@ -1,5 +0,0 @@
-class Business::AssessmentsController < ApplicationController
-  before_filter :require_user
-  before_filter :belongs_to_company
-  before_filter :trial_over
-  before_filter :company_deactivated?
