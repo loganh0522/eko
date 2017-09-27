7ad629ed6d1ac25392c98bc10cfe2b3ae7aac7eb
loganh0522 <loganh0522@gmail.com>
2017-09-27 08:19:34 -0400

    changes

--- app/models/job_board_header.rb
+++ app/models/job_board_header.rb
@@ -4,12 +4,6 @@ class JobBoardHeader < ActiveRecord::Base
   mount_uploader :logo, CareerPortalUploader
   mount_uploader :cover_photo, CareerPortalHeaderUploader
 
-  after_update :crop_avatar
-  
-  def crop_avatar
-    image.recreate_versions!(:large_image, :medium_image, :small_image) if crop_x.present?
-  end 
-
   # validates_size_of :logo, maximum: 500.kilobytes,
   #                   message: "should be no more than 500 KB", if: :image_changed?
 
