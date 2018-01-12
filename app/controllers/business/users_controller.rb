466e3ac78a302c143a9b4b7c597321cd32527b2d
loganh0522 <loganh0522@gmail.com>
2018-01-12 13:20:54 -0500

    changes

--- app/controllers/business/users_controller.rb
+++ app/controllers/business/users_controller.rb
@@ -38,7 +38,7 @@ class Business::UsersController < ApplicationController
         user_id: current_user.id
         )
       
-      # GoogleWrapper::Gmail.watch_gmail(current_user)
+      GoogleWrapper::Gmail.watch_gmail(current_user)
       redirect_to business_user_path(current_user)
     elsif params[:code].present? 
       token = get_token_from_code(params[:code])
