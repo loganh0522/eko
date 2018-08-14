e837a8873bda8356a6bf87deee8b4ba02aebd15f
Logan Houston <loganh0522@gmail.com>
2018-08-14 11:28:47 -0400

    changes

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -1,10 +1,13 @@
 class CompanyUser < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :company
+  belongs_to :user, :inverse_of => :company_users
+  belongs_to :company, :inverse_of => :company_users
 
-  validates_presence_of :user_id, :company_id
+  accepts_nested_attributes_for :user
+  validates_associated :user
 
-  validates
+
+  # accepts_nested_attributes_for :company
+  # validates_associated :company
   # validates :user_id, 
   #   :uniqueness => {:scope => [:assignable_id, :assignable_type]}
 end
